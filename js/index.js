bcbfa69aebcd1b4db9219e0b1b38c180c26acf23
Lizzie Paris <lzz.paris@gmail.com>
2017-02-28 22:43:17 -0800

    Fix double-quotes to single for AirBnB standards

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Provider = require("react-redux").Provider;
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Provider = require('react-redux').Provider;
 
-var routes = require("./components/routes");
+var routes = require('./components/routes');
 
-document.addEventListener("DOMContentLoaded", function() {
-  ReactDOM.render(routes, document.getElementById("app"));
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(routes, document.getElementById('app'));
 });
