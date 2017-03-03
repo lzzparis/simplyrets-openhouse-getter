326f0476c70d3b1feb79e1090e6e193c09659a46
Lizzie Paris <lzz.paris@gmail.com>
2017-03-02 19:12:31 -0800

    Use map in OpenHouseList, make stateful component

--- js/components/open-house-list.js
+++ js/components/open-house-list.js
@@ -2,17 +2,18 @@ var React = require('react');
 
 var OpenHouse = require('./open-house');
 
-var OpenHouseList = function OpenHouseList(props) {
-  var list = [];
-  props.listings.forEach(function addListing(listing, i) {
-    list.push(<OpenHouse key={i} listing={listing} />);
-  });
-  return (
-    <div className="open-house-list">
-      {list}
-    </div>
-  );
-};
+var OpenHouseList = React.createClass({
+  render: function() {
+    var list = this.props.listings.map(function addListing(listing) {
+      return <OpenHouse key={listing.mlsId} listing={listing} />;
+    });
+    return (
+      <div className="open-house-list">
+        {list}
+      </div>
+    );
+  }
+});
 
 
 module.exports = OpenHouseList;
