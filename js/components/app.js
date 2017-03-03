b08a2d94e34d35ebe3c78b712421e84277502e72
Lizzie Paris <lzz.paris@gmail.com>
2017-03-02 21:54:16 -0800

    Write mlsId to Firebase db on OpenHouse mount

--- js/components/app.js
+++ js/components/app.js
@@ -3,13 +3,13 @@ var React = require('react');
 var Banner = require('./banner');
 var OpenHouseListContainer = require('./open-house-list-container');
 
+
 var App = React.createClass({
   componentDidMount: function() {
     if (window.localStorage.getItem('userId') === null) {
       var userId = Math.floor(Math.random() * 1000000);
       window.localStorage.setItem('userId', userId);
     }
-
   },
   render: function() {
     return (
--- js/components/open-house.js
+++ js/components/open-house.js
@@ -1,31 +1,53 @@
 var React = require('react');
 var moment = require('moment'); 
 
-var OpenHouse = function OpenHouse(props) {
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0
-  });
+var firebase = require("firebase/app");
+require("firebase/database");
 
-  var backgroundImage = { 
-    backgroundImage: 'url(\'' + props.listing.photos[0] + '\')' 
-  };
-  var prettyListDate = moment(props.listing.listDate).format('MM-DD-YYYY');
-  var prettyPrice = formatter.format(props.listing.listPrice);
-
-  return (
-    <ul className="open-house">
-      <div className="open-house-image" style={backgroundImage}></div>
-      <li className="open-house-detail open-house-price">{prettyPrice}</li>
-      <li className="open-house-detail open-house-beds">Bedrooms: {props.listing.property.bedrooms}</li>
-      <li className="open-house-detail open-house-full-bath">Full baths: {props.listing.property.bathsFull}</li>
-      <li className="open-house-detail open-house-half-bath">Half baths: {props.listing.property.bathsHalf}</li>
-      <li className="open-house-detail open-house-stories">Stories: {props.listing.property.stories}</li>
-      <li className="open-house-detail open-house-mls-id">MLS ID: {props.listing.mlsId}</li>
-      <li className="open-house-detail open-house-list-date">Listed: {prettyListDate}</li>
-    </ul>
-  );
+var config = {
+  apiKey: "AIzaSyDfw6dgPvB7lRuCANYZVj-Bi67qmINZES4",
+  authDomain: "simplyrets-openhouse-getter.firebaseapp.com",
+  databaseURL: "https://simplyrets-openhouse-getter.firebaseio.com",
+  storageBucket: "simplyrets-openhouse-getter.appspot.com",
+  messagingSenderId: "410150604731"
 };
+
+firebase.initializeApp(config);
+
+var OpenHouse = React.createClass({
+  componentDidMount: function() {
+    var database = firebase.database();
+    var userId = window.localStorage.getItem('userId');  
+
+    database.ref('users/' + userId).push(this.props.listing.mlsId);
+  },
+  render: function() {
+    //TODO - refactor: move this business logic to redux side
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0
+    });
+
+    var backgroundImage = { 
+      backgroundImage: 'url(\'' + this.props.listing.photos[0] + '\')' 
+    };
+    var prettyListDate = moment(this.props.listing.listDate).format('MM-DD-YYYY');
+    var prettyPrice = formatter.format(this.props.listing.listPrice);
+
+    return (
+      <ul className="open-house">
+        <div className="open-house-image" style={backgroundImage}></div>
+        <li className="open-house-detail open-house-price">{prettyPrice}</li>
+        <li className="open-house-detail open-house-beds">Bedrooms: {this.props.listing.property.bedrooms}</li>
+        <li className="open-house-detail open-house-full-bath">Full baths: {this.props.listing.property.bathsFull}</li>
+        <li className="open-house-detail open-house-half-bath">Half baths: {this.props.listing.property.bathsHalf}</li>
+        <li className="open-house-detail open-house-stories">Stories: {this.props.listing.property.stories}</li>
+        <li className="open-house-detail open-house-mls-id">MLS ID: {this.props.listing.mlsId}</li>
+        <li className="open-house-detail open-house-list-date">Listed: {prettyListDate}</li>
+      </ul>
+    );
+  }
+});
 
 module.exports = OpenHouse;
