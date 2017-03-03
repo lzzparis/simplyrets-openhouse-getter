2bceb42ef5ba2a35d4b9cd651e47b51856a2ff0d
Lizzie Paris <lzz.paris@gmail.com>
2017-03-02 19:37:25 -0800

    Get API data on page load, print to console

--- js/actions/index.js
+++ js/actions/index.js
@@ -7,4 +7,31 @@ var resetState = function resetState() {
   };
 };
 
+var fetchOpenHouses = function fetchOpenHouses() {
+  return function(dispatch) {
+
+    var url = 'https://api.simplyrets.com/openhouses';
+    var headers = {
+        'Authorization': 'Basic '+btoa('simplyrets:simplyrets'),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    fetch(url, {
+      method: 'get',
+      headers: headers
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      var listings = response.map(function(openHouse) {
+        return openHouse.listing;
+      });
+      console.log(listings);
+    })
+  }
+
+};
+
 exports.RESET_STATE = RESET_STATE;
+exports.fetchOpenHouses = fetchOpenHouses;
--- js/components/open-house-list.js
+++ js/components/open-house-list.js
@@ -1,8 +1,13 @@
 var React = require('react');
 
+var actions = require('../actions/index');
+
 var OpenHouse = require('./open-house');
 
 var OpenHouseList = React.createClass({
+  componentWillMount: function() {
+    this.props.dispatch(actions.fetchOpenHouses());
+  },
   render: function() {
     var list = this.props.listings.map(function addListing(listing) {
       return <OpenHouse key={listing.mlsId} listing={listing} />;
